079c6d42b96d9ca08e91511a850f55240c63dda7
qzqaq <2925213501@qq.com>
2022-05-04 00:49:13 +0800

    0

--- repo/ksjsb4271.js
+++ repo/ksjsb4271.js
@@ -4,16 +4,6 @@
 IP代理：http://cxgc.top/archives/xiequdaili
 脚本兼容: QuantumultX, Surge,Loon, JSBox, Node.js
 =================================Quantumultx=========================
-重写：
-[task_local]
-#快手
-0 0-23/2 * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/ks.js, tag=快手, enabled=true
-[rewrite_local]
-appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ks.js
-ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ks.js
-[MITM]
-hostname = api.kuaisho*.com
-hostname = open.kuaisho*.com
 */
 const $ = new Env('快手极速版-IP代理');
 let res,
